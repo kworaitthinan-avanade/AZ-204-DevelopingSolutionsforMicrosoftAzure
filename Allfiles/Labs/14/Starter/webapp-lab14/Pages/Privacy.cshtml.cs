5550d28dca88d30f68c6ceda430e7b11cf49d06b
v-asamim <v-asamim@microsoft.com>
2023-10-12 16:47:52 +0100

    lab 15 renamed to lab 14

--- Allfiles/Labs/14/Starter/webapp-lab14/Pages/Privacy.cshtml.cs
+++ Allfiles/Labs/14/Starter/webapp-lab14/Pages/Privacy.cshtml.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace webapp_lab15.Pages;
+namespace webapp_lab14.Pages;
 
 public class PrivacyModel : PageModel
 {
